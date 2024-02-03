47c715b88c431069bb711a2914f8f1d90839f1d0
Anthony Blanco <anthony200527@outlook.com>
2024-02-02 16:17:24 -0800

    Schema.sql created

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create();
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 const sess = {
-    secret: '',
+    secret: process.env.SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: true,
